2d51f0b165a0392bcc190a1ff17b363303c86aa5
wuhaibin <wuhaibin@utry.com>
2022-05-12 22:40:35 +0800

    05_src_插件

---
+++ 05_src_插件/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <School />
+    <hr />
+    <Student />
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+import Student from './components/Student.vue'
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
